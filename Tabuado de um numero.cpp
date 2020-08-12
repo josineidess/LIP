62eafb6cd1e0d569337020116bcbc5651b2f2e4c
JosineideSS <josineide1402@gmail.com>
2020-08-12 05:16:17 -0300

    Cod: 41ed0937e6 Nível: Muito fácil

---
+++ Tabuado de um numero.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int main(){
+    cin>>n;
+    for(int e=1;e<= 10;e++){
+        cout<<n<< " x " << e<< " = " << n*e<<endl;
+    }
+    return 0;
+}
